4203e24f8676475bb939a08477e5025f3fde8cec
Ondřej Pacovský <op@eyen.se>
2025-07-07 17:13:48 +0200

    added hacky infer for v2

--- rtdetrv2_pytorch/tools/train.py
+++ rtdetrv2_pytorch/tools/train.py
@@ -1,19 +1,50 @@
-"""Copyright(c) 2023 lyuwenyu. All Rights Reserved.
+"""
+Copyright(c) 2023 lyuwenyu. All Rights Reserved.
 """
 
-import os 
-import sys 
+import os
+import sys
+from dataclasses import dataclass, field
+from typing import Optional, List
+
+from mashumaro.mixins.json import DataClassJSONMixin
+
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 
 import argparse
 
-from src.misc import dist_utils
+import neptune
 from src.core import YAMLConfig, yaml_utils
+from src.misc import dist_utils
 from src.solver import TASKS
 
 
+@dataclass
+class NeptuneConfiguration(DataClassJSONMixin):
+    """
+    Specifies Neptune.ai configuration for logging and tracking
+    """
+
+    # Neptune project name (ex. "Eyen/car-couplings").
+    # Will be set automatically using the model_type if None
+    project_name: Optional[str] = None
+    # Additional tags describing current run
+    tags: List[str] = field(default_factory=lambda: ["RT-DETR2"])
+
+def init_neptune(neptune_configuration: NeptuneConfiguration, model_type: str) -> neptune.Run:
+    """Initializes Neptune Run"""
+    project_name = neptune_configuration.project_name \
+        if neptune_configuration.project_name is not None \
+        else f"Eyen/{model_type.replace('_', '-')}"
+    os.environ["NEPTUNE_PROJECT"] = project_name
+    run = neptune.init_run(project=project_name, tags=neptune_configuration.tags)
+    # Export Neptune project name and run ID, so the same run can be accessed from multiple locations (ex. Loggers).
+    os.environ["NEPTUNE_CUSTOM_RUN_ID"] = run["sys/id"].fetch()
+    return run
+
 def main(args, ) -> None:
-    """main
+    """
+    Main
     """
     dist_utils.setup_distributed(args.print_rank, args.print_method, seed=args.seed)
 
@@ -28,19 +59,20 @@ def main(args, ) -> None:
     print('cfg: ', cfg.__dict__)
 
     solver = TASKS[cfg.yaml_cfg['task']](cfg)
-    
+    init_neptune(NeptuneConfiguration(), "car-feature-detector")
+
     if args.test_only:
         solver.val()
     else:
         solver.fit()
 
     dist_utils.cleanup()
-    
+
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    
+
     # priority 0
     parser.add_argument('-c', '--config', type=str, required=True)
     parser.add_argument('-r', '--resume', type=str, help='resume from checkpoint')
